55a15f769087aa1e7a2f1a68bfd2a8b548b6f387
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2022-05-23 13:57:45 +0300

    feat: init #4

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: [
+    'prod',
+    {
+      name: 'staging',
+      prerelease: 'beta',
+      channel: 'beta',
+    },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ]
+}
