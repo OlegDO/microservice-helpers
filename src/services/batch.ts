1a39e02cb7ac4fd836d41c765df3de4aea707184
Oleg Drozdovich <44732463+OlegDO@users.noreply.github.com>
2023-06-29 12:48:33 +0400

    feat(service): add batch find by chunks (#1)

---
+++ src/services/batch.ts
@@ -0,0 +1,37 @@
+import { SelectQueryBuilder } from 'typeorm';
+
+export interface IBatchFindOptions {
+  chunkSize?: number;
+}
+
+/**
+ * Batch service for typeorm
+ */
+class Batch {
+  /**
+   * Find entities and resolve callback
+   */
+  public static async find<TEntity>(
+    query: SelectQueryBuilder<TEntity>,
+    callback: (entities: TEntity[], index: number) => Promise<void> | void,
+    options: IBatchFindOptions = {},
+  ): Promise<void> {
+    const { chunkSize = 50 } = options;
+
+    let skip = 0;
+    let count = 0;
+    let index = 0;
+
+    do {
+      const chunkEntities = await query.skip(skip).take(chunkSize).getMany();
+
+      skip += chunkSize;
+      index++;
+      count = chunkEntities.length;
+
+      await callback(chunkEntities, index);
+    } while (count === chunkSize);
+  }
+}
+
+export default Batch;
